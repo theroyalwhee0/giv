54bcbe5c67dd99f76deeefb06f781d17836d50de
Adam Mill <hismajesty@theroyalwhee.com>
2025-08-29 15:06:12 -0400

    Update dependencies and enhance linting configuration; add clippy.toml for disallowed methods

--- bin/install.sh
+++ bin/install.sh
@@ -8,10 +8,10 @@ cargo test || exit 1
 # Install the binary.
 cargo install --path .
 
-# Generate the SHA256 checksum for the binary.
-(
-    cd target/release
-    sha256sum giv > giv.sha256
-)
-install_path=$(dirname $(which giv))
-cp target/release/giv.sha256 "$install_path/"
+# # Generate the SHA256 checksum for the binary.
+# (
+#     cd target/release
+#     sha256sum giv > giv.sha256
+# )
+# install_path=$(dirname $(which giv))
+# cp target/release/giv.sha256 "$install_path/"
