1ccb4c1098b137167457d0d02940c12349357876
Adam Mill <hismajesty@theroyalwhee.com>
2025-09-01 22:35:16 -0400

    Remove commented-out SHA256 checksum generation from install script

--- bin/install.sh
+++ bin/install.sh
@@ -10,11 +10,3 @@ cargo test || exit 1
 
 # Install the binary.
 cargo install --path .
-
-# # Generate the SHA256 checksum for the binary.
-# (
-#     cd target/release
-#     sha256sum giv > giv.sha256
-# )
-# install_path=$(dirname $(which giv))
-# cp target/release/giv.sha256 "$install_path/"
